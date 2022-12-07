3b6474aa4da8b46a1e891a29569ae32cc741a70f
irene <irenecagnoni91@gmail.com>
2022-12-07 17:04:19 +0100

    style: modified class names

--- components/Card/Card.js
+++ components/Card/Card.js
@@ -1,30 +1,41 @@
 import React from 'react';
 import styles from './Card.module.css';
+import Link from 'next/link';
 import { GoLocation } from 'react-icons/go';
 // import { SiTripadvisor } from 'react-icons/si';
 
 function Card({ restaurants }) {
   return restaurants.map((restaurant) => (
     <div key={restaurant.id} className={styles.card}>
-      <div className={styles.nameAverageScoreContainer}>
-        <h1>{restaurant.name}</h1>
-        <h2>{restaurant.averageScore}</h2>
+      <div className={styles['card__header']}>
+        <span className={styles['card__title']}>{restaurant.name}</span>
+        <span className={styles['card__title']}>{restaurant.averageScore}</span>
       </div>
-      <div className={styles.locationUrlContainer}>
-        <h3>
+
+      <div className={styles['card__body']}>
+        <span className={styles['card__subtitle']}>
           <GoLocation />
           {restaurant.location}
-        </h3>
-
-        <a href={restaurant.tripadvisorUrl}>Visit Tripadvisor page</a>
+        </span>
+        <Link href={restaurant.tripadvisorUrl}>Visit Tripadvisor page</Link>
       </div>
-      <ul className={styles.ratings}>
-        Ratings:
-        <li> Atmosphere: {restaurant.atmosphere} </li>
-        <li> Service: {restaurant.service} </li>
-        <li> Food: {restaurant.food} </li>
-        <li> Price: {restaurant.price} </li>
-      </ul>
+      <div className={styles['card__footer']}>
+        <span className={styles['card__list__title']}>RATINGS</span>
+        <ul className={styles['card__list']}>
+          <li className={styles['card__list__item']}>
+            Atmosphere: {restaurant.atmosphere}
+          </li>
+          <li className={styles['card__list__item']}>
+            Service: {restaurant.service}
+          </li>
+          <li className={styles['card__list__item']}>
+            Food: {restaurant.food}
+          </li>
+          <li className={styles['card__list__item']}>
+            Price: {restaurant.price}
+          </li>
+        </ul>
+      </div>
     </div>
   ));
 }
